137ab18dea284ffb6fba2e435d767924f30652f0
leesuyeon <sroovy@naver.com>
2021-12-29 16:09:26 +0900

    1000 solved!

---
+++ baekjoon/1단계 - 입출력과사칙연산/1000.js
@@ -0,0 +1,11 @@
+/* 
+🛼 문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+*/
+
+const fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(" ");
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
